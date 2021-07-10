4ca4395f60acbaa3938f0bd797d78a413eb01c71
hihunjin <hihunjin@hanmail.net>
2021-07-10 21:46:51 +0900

    initial commit

--- command.md
+++ command.md
@@ -1,1 +1,2 @@
+docker run -it --cpus=8 -p 3000:3000 -v $(pwd):/workspace python:3.8.11 /bin/bash
 sh setup.sh
--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-git clone https://github.com/magenta/ddsp
+# git clone https://github.com/magenta/ddsp
 cd ddsp
 apt update;
 apt-get -y install ffmpeg;
@@ -6,3 +6,4 @@ pip install -U pip;
 pip install tensorflow-cpu==2.5.0;
 pip install -e .[data_preparation,test] --use-deprecated=legacy-resolver
 cd ..
+pip install flask
